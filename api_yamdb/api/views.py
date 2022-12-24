31b6c041438819e2813067803c5ff11643917f6e
Армен Назаров <amnazaroff@gmail.com>
2022-12-25 00:06:12 +0500

    yep

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -10,9 +10,9 @@ from rest_framework.permissions import (IsAuthenticatedOrReadOnly,
                                         AllowAny, IsAuthenticated)
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
+
 from reviews.models import Category, Genre, Title, Review
 from users.models import User
-
 from .filters import TitlesSlugFilter
 from .mixins import CreateDestroyListViewSet
 from .permissions import (IsAdminOrReadOnly,
