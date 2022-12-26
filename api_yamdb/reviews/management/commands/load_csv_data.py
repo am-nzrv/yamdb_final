6767e726a22062b95291144c32af4d87724a6b2c
Армен Назаров <amnazaroff@gmail.com>
2022-12-27 00:43:57 +0500

    yep

--- api_yamdb/reviews/management/commands/load_csv_data.py
+++ api_yamdb/reviews/management/commands/load_csv_data.py
@@ -7,35 +7,35 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for row in DictReader(open('./static/data/users.csv',
-                                   encoding='latin-1')):
+                                   encoding='utf-8')):
             user = User(id=row['id'],
                         username=row['username'],
                         email=row['email'],
                         role=row['role'])
             user.save()
         for row in DictReader(open('./static/data/category.csv',
-                                   encoding='latin-1')):
+                                   encoding='utf-8')):
             category = Category(name=row['name'],
                                 slug=row['slug'])
             category.save()
         for row in DictReader(open('./static/data/genre.csv',
-                                   encoding='latin-1')):
+                                   encoding='utf-8')):
             genre = Genre(name=row['name'],
                           slug=row['slug'])
             genre.save()
         for row in DictReader(open('./static/data/titles.csv',
-                                   encoding='latin-1')):
+                                   encoding='utf-8')):
             title = Title(name=row['name'],
                           year=row['year'],
                           category_id=row['category'])
             title.save()
         for row in DictReader(open('./static/data/genre_title.csv',
-                                   encoding='latin-1')):
+                                   encoding='utf-8')):
             genre_title = GenreTitle(title_id=row['title_id'],
                                      genre_id=row['genre_id'])
             genre_title.save()
         for row in DictReader(open('./static/data/review.csv',
-                                   encoding='latin-1')):
+                                   encoding='utf-8')):
             review = Review(id=row['id'],
                             title_id=row['title_id'],
                             text=row['text'],
@@ -44,7 +44,7 @@ class Command(BaseCommand):
                             )
             review.save()
         for row in DictReader(open('./static/data/comments.csv',
-                                   encoding='latin-1')):
+                                   encoding='utf-8')):
             comments = Comments(id=row['id'],
                                 review_id=row['review_id'],
                                 text=row['text'],
